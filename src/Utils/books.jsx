b65bd81f48c949df06789f88a6286fb4e551eca3
Arifianafii <arifianafina05@gmail.com>
2025-04-16 23:50:19 +0700

    Tugas 3 ReactJS

--- src/Utils/books.jsx
+++ src/Utils/books.jsx
@@ -16,24 +16,28 @@ const books = [
     { 
         id: 3, 
         title: "Deep Work", 
+        year: 2020,
         description: "Rules for focused success in a distracted world",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS23bhRiy0NBtl7In1dc35mT6a4G9okHbfLtw&s" 
     },
     { 
         id: 4, 
         title: "Thinkin Fast and Slow", 
+        year: 2024,
         description: "cara kerja otak dalam berpikir dan mengambil keputusan",
         image : "https://www.bookmarked.id/cdn/shop/products/Thinking_FastandSlow2NEW_860x.jpg?v=1618044749"
     },
     { 
         id: 5, 
         title: "Sapiens", 
+        year: 2019,
         description: "A brief history of humankind",
         image : "https://assets-a1.kompasiana.com/items/album/2021/05/12/review-buku-sapiens-609bd5e08ede4809b31b5e52.png?t=o&v=780"
     },
     { 
         id: 6, 
         title: "Attitude Is Everything", 
+        year: 2021,
         description: "Hidupmu bakal sukses, kalau mentalmu beres",
         image : "https://down-id.img.susercontent.com/file/sg-11134201-22100-mgg0e1rhubjve7"
           },
