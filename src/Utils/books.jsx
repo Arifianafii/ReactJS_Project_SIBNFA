198609ab1279875d969a5bfaf6606229445ff529
Arifianafii <arifianafina05@gmail.com>
2025-04-18 00:40:57 +0700

    Tugas 3 ReactJS

--- src/Utils/books.jsx
+++ src/Utils/books.jsx
@@ -9,38 +9,59 @@ const books = [
     {
         id: 2,
         title: "Psychology of Money",
-        year: 2023,
+        year: 2020,
         description: "Timeless lessons on wealth, greed, and happines",
         image: "https://assets-a1.kompasiana.com/items/album/2022/01/12/morgan-housel-az-mmsmacjg-unsplash-scaled-61de39f606310e081b3b8de2.jpg?t=o&v=1200"
     },
     { 
         id: 3, 
         title: "Deep Work", 
-        year: 2020,
+        year: 2016,
         description: "Rules for focused success in a distracted world",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS23bhRiy0NBtl7In1dc35mT6a4G9okHbfLtw&s" 
     },
     { 
         id: 4, 
         title: "Thinkin Fast and Slow", 
-        year: 2024,
+        year: 2011,
         description: "cara kerja otak dalam berpikir dan mengambil keputusan",
         image : "https://www.bookmarked.id/cdn/shop/products/Thinking_FastandSlow2NEW_860x.jpg?v=1618044749"
     },
     { 
         id: 5, 
         title: "Sapiens", 
-        year: 2019,
+        year: 2011,
         description: "A brief history of humankind",
         image : "https://assets-a1.kompasiana.com/items/album/2021/05/12/review-buku-sapiens-609bd5e08ede4809b31b5e52.png?t=o&v=780"
     },
     { 
         id: 6, 
         title: "Attitude Is Everything", 
-        year: 2021,
+        year: 1999,
         description: "Hidupmu bakal sukses, kalau mentalmu beres",
         image : "https://down-id.img.susercontent.com/file/sg-11134201-22100-mgg0e1rhubjve7"
-          },
+    },
+    { 
+        id: 7, 
+        title: "The Power of Habit", 
+        year: 2012,
+        description: "Cara kebiasaan terbentuk dan bagaimana mengubahnya",
+        image : "https://www.bookmarked.id/cdn/shop/products/ThePowerofHabitNEW2_860x.jpg?v=1612547547"
+    },
+    { 
+        id: 8, 
+        title: "Start With Why", 
+        year: 2009,
+        description: "Pentingnya menemukan alasan utama dalam setiap tindakan",
+        image : "https://www.bookmarked.id/cdn/shop/products/StartwithWhyNEW_1024x.jpg?v=1607098849"
+    },
+    { 
+        id: 9, 
+        title: "Can’t Hurt Me", 
+        year: 2018,
+        description: "Ketangguhan mental untuk melewati batas diri",
+        image : "https://down-id.img.susercontent.com/file/id-11134207-7r98z-ltaelz884sex2b"
+    },
 ];
 
 export default books;
